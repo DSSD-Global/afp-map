75406e70bd1e40ebb1790a5a3e24d55555838916
Charlie Dobson <cmdobson@email.wm.edu>
2021-11-11 10:07:50 -0500

    Backend moved to Google Sheets

--- template/scripts/index.js
+++ template/scripts/index.js
@@ -30,3 +30,103 @@ const eqfeed_callback = function (results) {
         });
     }
 };
+
+// Client ID and API key from the Developer Console
+var CLIENT_ID = '459903889875-61v97luptt5b258jvahvffo4nmb85885.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyD8lCxi6u-p62TJvDKW7AnrQ_jrQuHyMOA';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
+
+// Authorization scopes required by the API; multiple scopes can be
+// included, separated by spaces.
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+
+
+var authorizeButton = document.getElementById('authorize_button');
+var signoutButton = document.getElementById('signout_button');
+
+function handleClientLoad() {
+
+    gapi.load('client:auth2', initClient);
+  }
+
+function initClient(){
+
+  gapi.client.init({
+    apiKey: API_KEY,
+    clientId: CLIENT_ID,
+    discoveryDocs: DISCOVERY_DOCS,
+    scope: SCOPES
+  }).then(function () {
+    // Listen for sign-in state changes.
+
+    gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+    // Handle the initial sign-in state.
+    updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+    document.getElementById('authorize_button').onclick = handleAuthClick;
+    document.getElementById('signout_button').onclick = handleSignoutClick;
+  }, function(error) {
+    appendPre(JSON.stringify(error, null, 2));
+  });
+}
+
+function updateSigninStatus(isSignedIn) {
+
+  if (isSignedIn) {
+    document.getElementById('authorize_button').style.display = 'none';
+    document.getElementById('signout_button').style.display = 'block';
+    listMembers();
+  } else {
+    document.getElementById('authorize_button').style.display = 'block';    
+    document.getElementById('signout_button').style.display = 'none';
+
+  }
+}
+
+
+/**
+       *  Sign in the user upon button click.
+       */
+ function handleAuthClick(event) {
+  gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+  gapi.auth2.getAuthInstance().signOut();
+}
+
+
+function appendPre(message) {
+  var pre = document.getElementById('content');
+  var textContent = document.createTextNode(message + '\n');
+  pre.appendChild(textContent);
+}
+
+  function listMembers() {
+    gapi.client.sheets.spreadsheets.values.get({
+  
+      spreadsheetId: '1Vfzjkiib0ZfwszfKoRdedp9QzXMopccnd2OZIwzqxvs',
+      range: 'B2:O136',
+    }).then(function(response) {
+      var range = response.result;
+      if (range.values.length > 0) {
+        for (i = 0; i < range.values.length; i++) {
+          var row = range.values[i];
+          for(j=0; j < row.length; j++){
+            console.log(row[j]);
+          }
+          console.log("");
+        }
+      } else {
+        appendPre('No data found.');
+      }
+    }, function(response) {
+      
+      appendPre('Error: ' + response.result.error.message);
+    });
+  }
